b8f0162f4e237e38db1bb9dc4ba7a4292769bbb6
Popov-AS <anton.popov.irk@gmail.com>
2024-03-31 02:48:16 +0300

    FEAT: Чтение файла конфигурации

---
+++ commons/global_vars.py
@@ -0,0 +1,3 @@
+config_path = "config/config.cfg"
+tinkoff_token: str
+telegram_token: str
---
+++ main.py
@@ -0,0 +1,21 @@
+from commons import global_vars
+from commons.cfg import get_param_from_cfg, read_config
+
+
+def check_config_params():
+    var_name = "tinkoff_token"
+    check_param = get_param_from_cfg("Main", var_name, print_error=True, terminate=True)
+    if check_param is not None:
+        global_vars.token = check_param
+
+    var_name = "telegram_token"
+    check_param = get_param_from_cfg("Main", var_name, print_error=True, terminate=True)
+    if check_param is not None:
+        global_vars.telegram_token = check_param
+
+
+if __name__ == '__main__':
+    # открываем файл конфигурации
+    read_config()
+    # применяем параметры из него
+    check_config_params()
