308ca2410ae0bb7f125521161b6c70fb70830652
Popov-AS <anton.popov.irk@gmail.com>
2024-04-02 18:14:55 +0300

    FIX: Время для события CandleEvent из BaseEvent

--- events/CandleEvent.py
+++ events/CandleEvent.py
@@ -6,3 +6,7 @@ from events.BaseEvent import BaseEvent
 @dataclass
 class CandleEvent(BaseEvent):
     figi: str
+
+    def __init__(self, figi: str):
+        super().__init__()
+        self.figi = figi
