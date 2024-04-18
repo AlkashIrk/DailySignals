fda5bae2e3f1144736eb0198314f43c90fe4673d
Popov-AS <anton.popov.irk@gmail.com>
2024-04-19 00:11:44 +0300

    FIX: Переподписка на стримы при отсутсвии данных.

--- commons/tinkoff/subscribe.py
+++ commons/tinkoff/subscribe.py
@@ -4,6 +4,7 @@ from time import sleep
 from tinkoff.invest import TradingStatus, MarketDataResponse, Candle
 from tinkoff.invest.market_data_stream.async_market_data_stream_manager import AsyncMarketDataStreamManager
 
+from commons.Timer import Timer
 from commons.instruments import load_from_csv
 from commons.tinkoff.api_v2 import authorize_async
 from events.config import init_event_bus
@@ -12,12 +13,17 @@ from model.data_structure.AuthData import AuthData
 from model.data_structure.SubsInstruments import SubsInstruments
 from model.repository.MemCandleRepository import MemCandleRepository
 
+watch_dog: Timer
+RECONNECT_TIMEOUT_SEC = 30
+WATCHDOG_TIMEOUT_SEC = 600
+
 
 def connect_to_api():
     """
     Подключение к API
     :return:
     """
+    global watch_dog
 
     # объект для авторизации
     auth = AuthData(token=Config().tinkoff_token)
@@ -37,9 +43,13 @@ def connect_to_api():
             MemCandleRepository.update_instruments(instruments.get_instrument_by_figi_dict())
             asyncio.run(subscribe(auth, instruments))
         except Exception as inst:
+            try:
+                watch_dog.cancel()
+            except Exception as e:
+                print("\t%s" % e)
             # в случае потери связи, переподписываемся через 30 секунд
             print("\t%s" % inst)
-            sleep(30)
+            sleep(RECONNECT_TIMEOUT_SEC)
 
 
 async def subscribe(auth: AuthData,
@@ -51,16 +61,17 @@ async def subscribe(auth: AuthData,
     :param instruments: список инструментов, для подписки
     :return:
     """
+    global watch_dog
 
     async with authorize_async(auth=auth) as client:
-        # отписка от всех
         market_data_stream: AsyncMarketDataStreamManager = (
             client.create_market_data_stream()
         )
-        market_data_stream.stop()
-        sleep(1)
 
-        # подпсика на свечи
+        # инициализация watch_dog подписки
+        watch_dog = Timer(WATCHDOG_TIMEOUT_SEC, timeout_callback, market_data_stream)
+
+        # подпиcка на свечи
         market_data_stream.candles.subscribe(instruments.get_subscribe_list_for_candles())
         sleep(1)
 
@@ -68,11 +79,24 @@ async def subscribe(auth: AuthData,
         market_data_stream.info.subscribe(instruments.get_subscribe_list_for_trade_info())
 
         async for market_data in market_data_stream:
+            # перезапуск watchdog на подписку
+            watch_dog.restart()
             data: MarketDataResponse = market_data
             if data.candle:
                 candle_event(data.candle)
             if data.trading_status:
                 info_event(data.trading_status)
+
+
+async def timeout_callback(streams: AsyncMarketDataStreamManager):
+    # отписка от всех стримов
+    print("timeout_callback")
+    try:
+        for stream in streams:
+            stream: AsyncMarketDataStreamManager = stream
+            stream.stop()
+    except Exception as e:
+        print("\t%s" % e)
 
 
 def candle_event(event: Candle):
